9e89e82f3b4675a79e6b755418b4e3fc8749c86a
Mohamed Al-Hussien <sn95racing.v6@gmail.com>
2021-03-31 21:56:21 -0400

    Add title and program description

---
+++ AssemblyIO/main.asm
@@ -0,0 +1,11 @@
+TITLE AssemblyIO
+
+; Author: Mohamed Al-Hussein
+; Description: 
+;     Program consists of custom macros to read and write strings to/from the console. 
+;     Also includes procedures to read an ASCII string integer and convert it to an integer, and vice versa.
+;     Includes macro for exception handling.
+;     Program starts with a test procedure that asks user for 10 digits before displaying them as an array, their sum,
+;     and their average.
+
+
